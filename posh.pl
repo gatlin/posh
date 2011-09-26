21257ac5795d58af70a1ac82999c7150f496ff21
Gatlin Johnson <rokenrol+github@gmail.com>
2011-09-26 17:23:17 -0500

    committing my first stab at posh

---
+++ posh.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+
+use v5.14;
+use strict;
+use warnings;
+
+my %env;
+%env = (
+    prompt => 'posh',
+);
+
+my %fun;
+%fun = (
+    sub => sub {
+        my $fn = shift;
+        $fun{$fn} = eval "sub { @_; }"
+    },
+    set => sub {
+        $env{$_[0]} = $_[1];
+    },
+    prompt => sub {
+        print "$env{prompt}> ";
+    },
+);
+
+sub do_cmd {
+    my ($fn, @args) = @_;
+    warn ("$fn not defined"), return if not defined $fun{$fn};
+    $fun{$fn}->(@args);
+}
+
+$fun{prompt}->();
+while (<>) {
+    my @cmds = split /\|/;
+    last if $cmds[0] =~ /^exit/;
+
+    my $in = "";
+    my $out;
+    {
+        local *STDIN;
+        local *STDOUT;
+        local *ARGV;
+        for my $cmd (@cmds) {
+            open STDIN, "<", \$in;
+            open STDOUT, ">", \$out;
+            *ARGV = *STDIN;
+            do_cmd split " ", $cmd;
+            close STDIN;
+            close STDOUT;
+            $in = $out;
+        }
+    }
+
+    print "$in\n" if $in;
+    $fun{prompt}->();
+}
+
+print "Goodbye!\n";
